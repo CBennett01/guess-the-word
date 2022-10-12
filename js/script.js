30457a82b0043bc01be559dff0ea91133ef9790f
bennett_c <bennettchristian01@gmail.com>
2022-10-12 08:02:21 -0500

    Accept and validate player guesses

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const guessesRemainingSpan = document.querySelector(".remaining span"); // The s
 const message = document.querySelector(".message"); // The empty paragraph where messages will appear when the player guesses a letter.
 const playAgainButton = document.querySelector(".play-again"); // The hidden button that will appear prompting the player to play again.
 const word = "magnolia";
+const guessedLetters = [];
 
 //Display symbols as placeholders for the chosen word's letters
 const tempDotts = function (word) {
@@ -22,8 +23,36 @@ const tempDotts = function (word) {
 tempDotts(word);
 
 guessButton.addEventListener("click", function (e) {
-    e.preventDefault();
+    e.preventDefault(); //keeps from reloading
+    message.InnerText = ""; //empties message paragraph
     const guess = letterInputBox.value;
-    console.log(guess);
+    const validatedGuess = guessValidater(guess);
+
+    if (validatedGuess) {
+        makeGuess(guess);
+    }
     letterInputBox.value = "";
 });
+
+const guessValidater = function (guess) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (guess.length === 0) {
+        message.innerText = "No guess received. Try guessing a letter.";
+    } else if (guess.length > 1) {
+        message.innerText = "Try entering only one letter.";
+    } else if (!guess.match(acceptedLetter)) {
+        message.innerText = "Please guess using only a letter.";
+    } else {
+        return guess;
+    }
+}
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You've already guessed that letter. Try again.";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
